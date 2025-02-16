1fa95fbb64b29da454965017d505aafc1d060754
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2025-02-16 15:28:36 +0800

    Add more test cases to testing convert_to_actual_movemement_time

--- ChandelierActualHeight_test.py
+++ ChandelierActualHeight_test.py
@@ -1,19 +1,32 @@
 import pytest
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from ChandelierActualHeight import convert_to_actual_movement_time
 
 @pytest.mark.parametrize(
     "previous_row, current_row, expected_output",
     [
-        # Test case format: (previous_row, current_row, expected_output)
-        (
+        #normal test
+        #cahnging days
+        #height is the same so no diff
+        #really small height change
+        #really big height change
+
+        Test case format: (previous_row, current_row, expected_output)
+        (   # Standard test case
             [datetime(2025, 1, 1, 1, 0, 0), 10], 
             [datetime(2025, 1, 1, 2, 0, 0), 15], 
             [[datetime(2025, 1, 1, 1, 57, 30), 10], [datetime(2025, 1, 1, 2, 2, 30), 15]]
         ),
-
-        # ((datetime(2025, 1, 1, 1, 0, 0), 20), (datetime(2025, 1, 1, 2, 0, 0), 10), 5.0, (20, 10)),
-        # ((datetime(2025, 1, 1, 3, 0, 0), 5), (datetime(2025, 1, 1, 4, 0, 0), 15), 5.0, (5, 15)),
+        (   # Changing days
+            [datetime(2025, 1, 1, 23, 0, 0), 20],
+            [datetime(2025, 1, 2, 00, 0, 0), 10],
+            [[datetime(2025, 1, 1, 23, 55, 0), 20], [datetime(2025, 1, 2, 00, 5, 0), 10]]
+        ),
+        (   # Changing years
+            [datetime(2025, 12, 31, 23, 0, 0), 20],
+            [datetime(2026, 1, 1, 00, 0, 0), 30],
+            [[datetime(2025, 12, 31, 23, 55, 0), 20], [datetime(2026, 1, 1, 00, 5, 0), 30]]
+        ),
     ]
 )
 
